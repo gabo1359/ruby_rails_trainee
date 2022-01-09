f7cda71dd2b0a68e029f02ead01f24c140d73ad9
Gabriel <gabs1995@hotmail.com>
2022-01-09 13:27:23 -0500

    Solved: Problem 1. GL

--- week1/problem1/main.rb
+++ week1/problem1/main.rb
@@ -1,6 +1,8 @@
-require_relative './view.rb'
+# frozen_string_literal: false
 
-alphabetical_increase =  ("aa".."zz").to_a
+require_relative './view'
+
+alphabetical_increase = ('aa'..'zz').to_a
 total_elements = alphabetical_increase.length
 
 view(total_elements, alphabetical_increase)
