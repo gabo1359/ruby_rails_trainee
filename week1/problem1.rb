8bb9e57975a790d2175b0310d89cc396c32814db
Gabriel <gabs1995@hotmail.com>
2022-01-09 11:39:21 -0500

    Solved: Problem 1. GL

---
+++ week1/problem1.rb
@@ -0,0 +1,8 @@
+require_relative './problem1_menu.rb'
+
+alphabetical_increase =  ("aa".."zz").to_a
+total_elements = alphabetical_increase.length
+
+problem1_menu(total_elements, alphabetical_increase)
+
+
---
+++ week1/problem1_menu.rb
@@ -0,0 +1,34 @@
+def problem1_menu(total_elements, alphabetical_increase)
+  title()
+  elements = asked_for_elements(total_elements)
+  response(alphabetical_increase, elements)
+end
+
+def title
+  puts
+  puts "***********************************************"
+  puts "*                  Problem 1                  *"
+  puts "***********************************************"
+  puts
+end
+
+def asked_for_elements(total_elements)
+  puts "How many elements do you want to see (1 - #{total_elements})?"
+  print ">> "
+  elements = gets.chomp.to_i
+  while elements <= 0 || elements > total_elements
+    puts
+    puts "Incorrect. Write a number between 1 and #{total_elements}:"
+    print ">> "
+    elements = gets.chomp.to_i
+  end
+  elements
+end
+
+def response(alphabetical_increase, elements)
+  puts
+  puts "Showing #{elements} element#{"s" if elements > 1}:"
+  puts
+  puts alphabetical_increase[0..(elements - 1)].join(", ") << "."
+  puts
+end
